c241c496ab9bb41bdb7ba2ba236166b09a1b266e
Muhammad Ali Hamza <wordpresswithali44@gmail.com>
2026-02-13 09:58:49 +0500

    Basic Conditional Problems

---
+++ Conditional_Problems/fruit.py
@@ -0,0 +1,10 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Invalid")
+    elif color == "Yellow":
+        print("valid")
+    elif color == "Purpule":
+        print("Invalid")
---
+++ Conditional_Problems/ticket_price.py
@@ -0,0 +1,12 @@
+age = 29
+day = "wednesday"
+
+price = 12 if age <= 18 else 25 
+
+print(price)
+
+if day == "wednesday":
+    price -= 2
+
+print( "Ticket price for you is $", price )
+
