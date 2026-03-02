4e3e523caaf502510c094de9227ef4ed60c9bc71
Muhammad Ali Hamza <wordpresswithali44@gmail.com>
2026-03-02 11:35:15 +0500

    basic python poem

---
+++ basic_python/basic_code.py
@@ -0,0 +1,27 @@
+Poem = '''This was the year breath became death.
+The year the grandparents and great grandparents left us by the thousands, taking their wisdom with them into the great oxygen mask of the heavens. This was the year we covered our mouths and smiled with our eyes if we were able to smile at all.
+This year handshakes became passé and hugs a mass hallucination we once had.
+This was the year we transformed into a tribe of screens.
+The year we finally acknowledged how crucial delivery drivers and supermarket stock clerks are. First responders heroes now more than ever.
+This was the year we remembered how to bake bread, learned how to garden.
+The year we discovered different species of trees possess distinctive aromas.
+This was the year we binge watched and wore out pajamas.
+The year we took our pens up and our notebooks seriously.
+The year we were speechless.
+This was the year we discovered the Zen of handwashing, observed all the delicate shadows the moon tats across night lawns.
+This was the year we cancelled weddings, Christmas, died without anyone familiar by our sides.
+But this was also the year we took unscheduled strolls in the forest alone, attended the sea’s susurrus lectures, hiked higher than ever before.
+The year we feared air and loved air and drove less, thus clearing the air. This was the year we did more fretting, more regretting.
+We vowed to vote and kept our promises to ourselves and others.
+This was the year we stood up to racial injustice in the myriad ways we could—in the streets, on Facebook, writing to our senators, calling for action against the police, donating to causes, celebrating artists of color, holding our white tongues so the underrepresented could be heard, acknowledged, admired.
+This was the year we used our phones to make actual calls, voice to voice, not just for texts and emojis.
+The year we cried and shook our heads and wrung our hands at the headlines.
+This was the year we lost sleep, lost heart, found hope is action.
+This is the year we said I love you over and over.
+Sometimes to the person on the other side of the glass.
+Sometimes to songbirds.
+Sometimes to ourselves.
+The year we said I love you to our fragile Earth.
+Said I love you, I love you to the universe, and I love my humble place in it, no matter what. '''
+
+print(Poem)
