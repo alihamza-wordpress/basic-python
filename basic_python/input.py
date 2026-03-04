7eb91e08479440388abd54d76f9cd9255e981319
Muhammad Ali Hamza <wordpresswithali44@gmail.com>
2026-03-04 11:17:35 +0500

    input function

---
+++ basic_python/input.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number 1: "))
+b = int(input("Enter a number 2: "))
+
+print("A number is: ", a)
+print("A number is: ", b)
+print("A product is: ", a * b)
+
---
+++ basic_python/string.py
@@ -0,0 +1,10 @@
+Name = "Ali Hamza"
+
+Length = len(Name)
+shortname = Name[0:6]
+
+character1 = Name[7]
+
+print(Length)
+print(shortname)
+print(character1)
